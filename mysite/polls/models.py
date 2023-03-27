ab0b77aabeadb255f1aa947391d1cd3fbe290a31
Reed Rodgers <reed.rodgers@gmail.com>
2023-03-26 23:37:12 -0300

    Mission Complete.

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -3,15 +3,13 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
-
 class Question(models.Model):
     def __str__(self):
         return self.question_text
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
-
+    question_text = models.CharField(max_length=200, unique=True)
+    pub_date = models.DateTimeField('date published') 
 
 class Choice(models.Model):
     def __str__(self):
@@ -19,4 +17,9 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+    
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['question', 'choice_text'], name='unique_question_choice_text')
+        ]
         
